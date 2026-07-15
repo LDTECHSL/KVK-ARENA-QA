04fd9cd8ba213a59b2708d92294cb800591163b1
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-07-15 11:30:03 +0530

    fdhgf

--- src/env.ts
+++ src/env.ts
@@ -5,5 +5,11 @@ export const getEnv = () => {
     API_URL: env?.API_URL ?? "",
     BASE_URL: env?.BASE_URL ?? "",
     OFFER_END_DATE: env?.OFFER_END_DATE ?? "",
+    MEMBERSHIP_PRICE: env?.MEMBERSHIP_PRICE ?? 0,
+    GYM_RATE: env?.GYM_RATE ?? 0,
+    BADMINTON_RATE: env?.BADMINTON_RATE ?? 0,
+    CARWASH_RATE: env?.CARWASH_RATE ?? 0,
+    GAMING_RATE: env?.GAMING_RATE ?? 0,
+    CAFE_RATE: env?.CAFE_RATE ?? 0,
   };
 };
